313de825d674fbee460c29fe71ca2bf78058ab9c
melodic456 <137816046+melodic456@users.noreply.github.com>
2023-10-21 12:20:34 +0600

    Update app.py

--- app.py
+++ app.py
@@ -46,15 +46,97 @@ def get_binance_price_ticker2():
     response = requests.get(url)
     data = response.json()
     #print(data)
-    pair_id = 0
-    pair = ''
+    pair_id = 7223
+    pair = 'USDT'
     last_trade_binance = 0
+    if pair == "USDT":
+        pair_id = 7223
+        cookies = {
+            '__ddg1_': 'p4pod51GNc8M8FBo8wiH',
+            'locale': 'en',
+            'c9839b948ceea6f557b8912b7229bec8': '1',
+            'registertimer': '1',
+            'LLXR': '1695900086',
+            'LLXUR': 'ec99ee9e6bfb',
+            '_ym_uid': '1695900088911625923',
+            '_ym_d': '1695900088',
+            '_ym_isad': '1',
+            'marketbase': 'usd',
+            'Rfr': 'https%3A%2F%2Fyobit.net%2Fen%2F',
+            '__ddgid_': 'ae9EHVtYzWt9vWvD',
+            '__ddgmark_': 'WCNb3BI9y8qb6YtV',
+            '__ddg5_': 'a1KHVYth3P0yA19g',
+            'PHPSESSID': 'bipggtgo5t7cqvnk89f3hgh0vc',
+        }
+
+        headers = {
+            'authority': 'yobit.net',
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'accept-language': 'en-US,en;q=0.9,bn;q=0.8',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            # 'cookie': '__ddg1_=p4pod51GNc8M8FBo8wiH; locale=en; c9839b948ceea6f557b8912b7229bec8=1; registertimer=1; LLXR=1695900086; LLXUR=ec99ee9e6bfb; _ym_uid=1695900088911625923; _ym_d=1695900088; _ym_isad=1; marketbase=usd; Rfr=https%3A%2F%2Fyobit.net%2Fen%2F; __ddgid_=ae9EHVtYzWt9vWvD; __ddgmark_=WCNb3BI9y8qb6YtV; __ddg5_=a1KHVYth3P0yA19g; PHPSESSID=bipggtgo5t7cqvnk89f3hgh0vc',
+            'origin': 'https://yobit.net',
+            'referer': 'https://yobit.net/en/trade/' + str(pair).upper() + '/USD',
+            'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+        }
+
+        datas = {
+            'pair_id': str(pair_id),
+            'tz': 'Asia/Dhaka',
+        }
+
+        response = requests.post('https://yobit.net/ajax/system_status_data.php', cookies=cookies,
+                                 headers=headers,
+                                 data=datas)
+        # print(response.json())
+        for i in range(len(response.json()['buyord'])):
+            # #print(response.json()['buyord'][i]['p'], response.json()['buyord'][i]['a'])
+            # #print(response.json()['buyord'][i]['p'], response.json()['buyord'][i]['a'])
+            last_trade = float(response.json()['buyord'][i]['p'])
+            print(last_trade)
+            ltc_amount = float(response.json()['buyord'][i]['a'])
+            # calculate the difference
+            # send the message to telegram
+
+            # считаем разницу
+
+            percent = ((last_trade - last_trade_binance) / last_trade_binance) * int(100)
+            print(percent)
+            # percent = int(100) / (last_trade - last_trade_binance) / last_trade_binance * int(100000)
+            percent2 = round(percent, 4)
+            # #print('Difference between exchange rates in %', percent2)
+            # time.sleep(1)
+            binance_1000 = 1000 / last_trade_binance
+            yobit_1000 = binance_1000 * last_trade
+            # #print(yobit_1000)
+            # config = configparser.ConfigParser()
+            # config.read('config.ini')
+            # crypto_value = config.get('Section 1', str(pair))
+            # crypto_amount = config.get('Section 3', str(pair) + "_a")
+
+            # if yobit_1000 > int(crypto_value) and float(ltc_amount) > float(crypto_amount):
+                # percent_str = str(percent2) + ' The difference has reached the purchase level!!!! > 7%' + str()
+            percent_str = str(pair).upper() + ' : The difference is now ' + str(
+                percent2) + '%' + '\nBinance rate: ' + str(last_trade_binance) + \
+                          "\nYobit rate: " + str(last_trade) + "\n" + str(pair).upper() + " amount: " + str(
+                ltc_amount) + "\nYobit for 1000 , rate is " + str(yobit_1000)
+            # print(percent_str)
+            # if percent2 < 7 or percent2 > 10:
+            # send_msg(percent_str)
+            return percent_str
     for sp_data in data:
-        pair = sp_data['name'].replace("USDT", "")
+        pair = sp_data['name'].replace("USD", "")
         print(pair)
         for symbol, value in crypto_values.items():
             print(f"{symbol}: {value}")
-            if symbol == pair.upper():
+            if symbol in pair.upper():
                 # pair = symbol.replace("USDT", "")
                 pair_id = value
                 last_trade_binance = float(sp_data['value'])
@@ -151,12 +233,94 @@ def get_specific_crypto_price_ticker(crypto_name):
     last_trade_binance = 0
     pair = crypto_name.upper()
     print(pair)
+    if pair == "USDT":
+        pair_id = 7223
+        cookies = {
+            '__ddg1_': 'p4pod51GNc8M8FBo8wiH',
+            'locale': 'en',
+            'c9839b948ceea6f557b8912b7229bec8': '1',
+            'registertimer': '1',
+            'LLXR': '1695900086',
+            'LLXUR': 'ec99ee9e6bfb',
+            '_ym_uid': '1695900088911625923',
+            '_ym_d': '1695900088',
+            '_ym_isad': '1',
+            'marketbase': 'usd',
+            'Rfr': 'https%3A%2F%2Fyobit.net%2Fen%2F',
+            '__ddgid_': 'ae9EHVtYzWt9vWvD',
+            '__ddgmark_': 'WCNb3BI9y8qb6YtV',
+            '__ddg5_': 'a1KHVYth3P0yA19g',
+            'PHPSESSID': 'bipggtgo5t7cqvnk89f3hgh0vc',
+        }
+
+        headers = {
+            'authority': 'yobit.net',
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'accept-language': 'en-US,en;q=0.9,bn;q=0.8',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            # 'cookie': '__ddg1_=p4pod51GNc8M8FBo8wiH; locale=en; c9839b948ceea6f557b8912b7229bec8=1; registertimer=1; LLXR=1695900086; LLXUR=ec99ee9e6bfb; _ym_uid=1695900088911625923; _ym_d=1695900088; _ym_isad=1; marketbase=usd; Rfr=https%3A%2F%2Fyobit.net%2Fen%2F; __ddgid_=ae9EHVtYzWt9vWvD; __ddgmark_=WCNb3BI9y8qb6YtV; __ddg5_=a1KHVYth3P0yA19g; PHPSESSID=bipggtgo5t7cqvnk89f3hgh0vc',
+            'origin': 'https://yobit.net',
+            'referer': 'https://yobit.net/en/trade/' + str(pair).upper() + '/USD',
+            'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+        }
+
+        datas = {
+            'pair_id': str(pair_id),
+            'tz': 'Asia/Dhaka',
+        }
+
+        response = requests.post('https://yobit.net/ajax/system_status_data.php', cookies=cookies,
+                                 headers=headers,
+                                 data=datas)
+        # print(response.json())
+        for i in range(len(response.json()['buyord'])):
+            # #print(response.json()['buyord'][i]['p'], response.json()['buyord'][i]['a'])
+            # #print(response.json()['buyord'][i]['p'], response.json()['buyord'][i]['a'])
+            last_trade = float(response.json()['buyord'][i]['p'])
+            print(last_trade)
+            ltc_amount = float(response.json()['buyord'][i]['a'])
+            # calculate the difference
+            # send the message to telegram
+
+            # считаем разницу
+
+            percent = ((last_trade - last_trade_binance) / last_trade_binance) * int(100)
+            print(percent)
+            # percent = int(100) / (last_trade - last_trade_binance) / last_trade_binance * int(100000)
+            percent2 = round(percent, 4)
+            # #print('Difference between exchange rates in %', percent2)
+            # time.sleep(1)
+            binance_1000 = 1000 / last_trade_binance
+            yobit_1000 = binance_1000 * last_trade
+            # #print(yobit_1000)
+            # config = configparser.ConfigParser()
+            # config.read('config.ini')
+            # crypto_value = config.get('Section 1', str(pair))
+            # crypto_amount = config.get('Section 3', str(pair) + "_a")
+
+            # if yobit_1000 > int(crypto_value) and float(ltc_amount) > float(crypto_amount):
+                # percent_str = str(percent2) + ' The difference has reached the purchase level!!!! > 7%' + str()
+            percent_str = str(pair).upper() + ' : The difference is now ' + str(
+                percent2) + '%' + '\nBinance rate: ' + str(last_trade_binance) + \
+                          "\nYobit rate: " + str(last_trade) + "\n" + str(pair).upper() + " amount: " + str(
+                ltc_amount) + "\nYobit for 1000 , rate is " + str(yobit_1000)
+            # print(percent_str)
+            # if percent2 < 7 or percent2 > 10:
+            # send_msg(percent_str)
+            return percent_str
     for sp_data in data:
         # pair = sp_data['name'].replace("USDT", "")
 
 
         for symbol, value in crypto_values.items():
-            if symbol == pair.upper() and pair.upper() in sp_data['name']:
+            if symbol == pair.upper() and pair.upper() in sp_data['name'].replace("USD", ""):
                 print(sp_data['name'], sp_data['value'])
                 print(f"{symbol}: {value}")
                 # pair = symbol.replace("USDT", "")
